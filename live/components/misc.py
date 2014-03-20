d766befe247e71698087c0b8c7c28ee0574f0074
GalanCM <galan.c.m@gmail.com>
2014-03-19 23:13:30 -0400

    Restructure component library and fix move_to
    Compent library now breaks up components by type: motion, misc, control,
    and behavior. in addition move_to's accel branch now properly converts
    the receive per second current_speed and max_speed values into per frame
    values for use in applyMovement. This approach is used instead of using
    applyMovement's per_second keyword to prevent recalculating the per
    second value when checking if the object has reached its location.

---
+++ live/components/misc.py
@@ -0,0 +1,30 @@
+from live.helpers import Timer
+
+def timed(fun, time, next_fun=None):
+	"""A component that runs another component for a fixed length of time. Can optionally be given a follow-up component for chaining.
+
+	   :param callable fun: The component to be run:
+	   :param number time: The amount of time to run the component
+	   :keyword callable next_fun: A component to run after the timed component is finished
+	"""
+	timer = Timer(time)
+	def timed_callback(self, id):
+		nonlocal timer
+		if timer > 0.0:
+			fun(self, id)
+		else:
+			if next_fun:
+				self.logic_components.set(next_fun, id=id)
+			else:
+				self.logic_components.remove(id)
+	return timed_callback
+
+def suspend(time, next_fun):
+	"""A component that suspends a component currently in the component list for a fixed length of time. Can optionally be given a different component to be run after the suspension is lifted.
+
+	   :param number time: The amount of time to run the component
+	   :keyword callable next_fun: A component to run after the suspension is lifted
+	"""
+	def suspend_callback(self, id):
+		pass
+	return timed(suspend_callback, time, next_fun )
--- live/components/motion.py
+++ live/components/motion.py
@@ -1,36 +1,6 @@
-from live.helpers import Timer
 from bge import logic
 
 from mathutils import Vector
-
-def timed(fun, time, next_fun=None):
-	"""A component that runs another component for a fixed length of time. Can optionally be given a follow-up component for chaining.
-
-	   :param callable fun: The component to be run:
-	   :param number time: The amount of time to run the component
-	   :keyword callable next_fun: A component to run after the timed component is finished
-	"""
-	timer = Timer(time)
-	def timed_callback(self, id):
-		nonlocal timer
-		if timer > 0.0:
-			fun(self, id)
-		else:
-			if next_fun:
-				self.logic_components.set(next_fun, id=id)
-			else:
-				self.logic_components.remove(id)
-	return timed_callback
-
-def suspend(time, next_fun):
-	"""A component that suspends a component currently in the component list for a fixed length of time. Can optionally be given a different component to be run after the suspension is lifted.
-
-	   :param number time: The amount of time to run the component
-	   :keyword callable next_fun: A component to run after the suspension is lifted
-	"""
-	def suspend_callback(self, id):
-		pass
-	return timed(suspend_callback, time, next_fun )
 
 def move_to(obj, target, **kwargs):
 	"""Move the object to a target position. Can be done using speed, time, or acceleration, but no combination of them.
@@ -47,13 +17,14 @@ def move_to(obj, target, **kwargs):
 	   :keyword number start_speed: (optional) (for acceleration) Initial speed for the object to move at. In blender units per second.
 	   :keyword number max_speed: (optional) (for acceleration) Speed at which to stop acceleration. In blender units per second.
 	"""
+
 	if 'accel' in kwargs:
 		for key in kwargs.keys():
 			if key not in ('accel', 'start_speed', 'max_speed'):
 				raise TypeError('%s is an invalid keyword argument for this function' % key)
 		accel = kwargs['accel']
-		current_speed = kwargs['start_speed'] if 'start_speed' in kwargs else 0.0
-		max_speed = kwargs['max_speed'] if 'max_speed' in kwargs else False
+		current_speed = kwargs['start_speed'] / (logic.getLogicTicRate()+1.0) if 'start_speed' in kwargs else 0.0
+		max_speed = kwargs['max_speed'] / (logic.getLogicTicRate()+1.0) if 'max_speed' in kwargs else False
 
 		target = Vector(target)
 		stop_next = False
@@ -77,7 +48,7 @@ def move_to(obj, target, **kwargs):
 		for key in kwargs:
 			if key != 'speed':
 				raise TypeError('%s is an invalid keyword argument for this function' % key)
-		speed = kwargs['speed'] / (logic.getLogicTicRate()+1.0)
+		speed = kwargs['speed']
 		return move_to(obj, target, accel=0, start_speed=speed, max_speed=speed)
 
 	elif 'time' in kwargs:
@@ -86,5 +57,8 @@ def move_to(obj, target, **kwargs):
 				raise TypeError('%s is an invalid keyword argument for this function' % key)
 		speed = obj.getDistanceTo(target) / kwargs['time']
 		return move_to(obj, target, speed=speed)
+
+	else:
+		raise TypeError('Second argument must be named "time", "speed", or "accel".')
 
 	return move_callback

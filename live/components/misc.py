1ff19fcd0bcbb396b7cb676c5dddf8d3c8652419
GalanCM <galan.c.m@gmail.com>
2014-04-29 21:42:57 -0400

    Update timed_callback to support collision callbacks.

--- live/components/misc.py
+++ live/components/misc.py
@@ -8,15 +8,20 @@ def timed(fun, time, next_fun=None):
 	   :keyword callable next_fun: A component to run after the timed component is finished
 	"""
 	timer = Timer(time)
-	def timed_callback(self, id):
+	def timed_callback(self, id, *args):
 		nonlocal timer
 		if timer > 0.0:
 			fun(self, id)
 		else:
-			if next_fun:
-				self.logic_components.set(next_fun, id=id)
+			if len(args) == 0:
+				correct_queue = self.logic_components
 			else:
-				self.logic_components.remove(id)
+				correct_queue = self.collision_components
+
+			if next_fun:
+				correct_queue.set(next_fun, id=id)
+			else:
+				correct_queue.remove(id)
 	return timed_callback
 
 def suspend(time, next_fun):
